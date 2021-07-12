c74c179db31a397539c36ffa01833b6ba1b5482d
bhargavpappu <bpappu12@gmail.com>
2021-07-12 13:29:38 -0400

    ArcTangent implementation to find angle of the joystick for player movement.

--- app/src/main/java/com/gamecodeschool/joystick/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/joystick/MainActivity.java
@@ -16,5 +16,31 @@ public class MainActivity extends AppCompatActivity implements JoystickView.Joys
     public void onJoystickMoved(float xCoordinate, float yCoordinate, int id)
     {
         Log.d("Main Method","X Coordinate: " + xCoordinate + "Y Coordinate: " + yCoordinate);
+        if(xCoordinate > 0 && yCoordinate > 0)
+        {
+            double angle;
+            angle = (Math.atan(yCoordinate/xCoordinate)) * (180/Math.PI);
+            Log.d("Main Method", "Angle: " + angle);
+        }
+        if (xCoordinate < 0 && yCoordinate > 0 )
+        {
+            double angle;
+            angle = (Math.atan(yCoordinate/xCoordinate)) * (180/Math.PI) + 180;
+            Log.d("Main Method", "Angle: " + angle);
+        }
+        if (xCoordinate < 0 && yCoordinate < 0 )
+        {
+            double angle;
+            angle = (Math.atan(yCoordinate/xCoordinate)) * (180/Math.PI) + 180;
+            Log.d("Main Method", "Angle: " + angle);
+        }
+
+        if (xCoordinate > 0 && yCoordinate < 0 )
+        {
+            double angle;
+            angle = (Math.atan(yCoordinate/xCoordinate)) * (180/Math.PI) + 360;
+            Log.d("Main Method", "Angle: " + angle);
+        }
+
     }
 }
